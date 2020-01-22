e1d5cc6e499bbcc0dbf476620dfe1cc755ce8e98
Justin Fu <56492118+justin226@users.noreply.github.com>
2020-01-22 10:32:12 -0800

    Create help.js

---
+++ help.js
@@ -0,0 +1,3 @@
+var help = document.createElement("style");
+document.head.appendChild(help);
+help.innerHTML = "red{color: red} orange{color:orange} yellow{color: yellow} green{color: green} blue{color: blue} purple{color: purple}";
