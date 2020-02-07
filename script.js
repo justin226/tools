9d964c3c3439f5b52551729f41a675725e903ac6
Justin Fu <56492118+justin226@users.noreply.github.com>
2020-02-07 14:40:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,29 @@ document.getElementById("darkmode").addEventListener("change", function(){
 });
 document.getElementById("save").addEventListener("click", function(){
   var item = prompt("Save to...");
-  /*HTML*/localStorage.setItem(item + " html", document.getElementById("html").value);
-  /*CSS*/localStorage.setItem(item + " css", document.getElementById("css").value);
-  /*JS*/localStorage.setItem(item + " js", document.getElementById("js").value);
+  if(item + " html" != null){
+    /*HTML*/localStorage.setItem(item + " html", document.getElementById("html").value);
+    /*CSS*/localStorage.setItem(item + " css", document.getElementById("css").value);
+    /*JS*/localStorage.setItem(item + " js", document.getElementById("js").value);
+  } else {
+    if(confirm("Are you sure?\nThis will overwrite your last save.")){
+      /*HTML*/localStorage.setItem(item + " html", document.getElementById("html").value);
+      /*CSS*/localStorage.setItem(item + " css", document.getElementById("css").value);
+      /*JS*/localStorage.setItem(item + " js", document.getElementById("js").value);
+    }
+  }
+});
+document.getElementById("delete").addEventListener("click", function(){
+  var item = prompt("Delete...");
+  if(localStorage.getItem(item + " html") != null){
+    if(confirm("Are you sure?\nThis action cannot be undone.")){
+      /*HTML*/localStorage.removeItem(item + " html");
+      /*CSS*/localStorage.removeItem(item + " css");
+      /*JS*/localStorage.removeItem(item + " js");
+    }
+  } else {
+    alert("Not Found");
+  }
 });
 document.getElementById("load").addEventListener("click", function(){
   var item = prompt("Load...");
@@ -25,6 +45,6 @@ document.getElementById("load").addEventListener("click", function(){
     document.getElementById("css").value = localStorage.getItem(item + " css");
     document.getElementById("js").value = localStorage.getItem(item + " js");
   } else {
-    alert("Not Found")
+    alert("Not Found");
   }
 });
